4719d28938dd873e8a39e080b7116aae5951448c
Gabriel Ramirez <luga.ramirez@gmail.com>
2015-11-30 02:29:35 -0300

    Finished the first task and added some extra tidbits to improve the UX

--- media.py
+++ media.py
@@ -1,8 +1,10 @@
 import webbrowser
 
 class Movie():
-  def __init__(self, title, storyline, poster_image, trailer_youtube):
+  def __init__(self, title, genre, storyline, poster_image, trailer_youtube):
     self.title = title
+    self.tag = genre.replace(" ", "")
+    self.genre = genre
     self.storyline = storyline
     self.poster_image_url = poster_image
     self.trailer_youtube_url = trailer_youtube
