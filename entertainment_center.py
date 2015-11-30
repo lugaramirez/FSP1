c652e886ce4a026ee471d42cf051dbfae9216de6
Gabriel Ramirez <gabriel.ramirez@santcoventure.com>
2015-11-29 21:22:25 -0300

    First version of all the files

---
+++ entertainment_center.py
@@ -0,0 +1,13 @@
+import media
+
+interstellar = media.Movie("Interstellar",
+	"test 1",
+	"http://meetinthelobby.com/wp-content/uploads/2014/11/Interstellar-Teaser-Movie-Poster-Large.jpg",
+	"https://www.youtube.com/watch?v=0vxOhd4qlnA")
+
+prometheus = media.Movie("Prometheus",
+	"test 2",
+	"http://www.gstatic.com/tv/thumb/movieposters/8815605/p8815605_p_v7_ad.jpg",
+	"https://www.youtube.com/watch?v=HHcHYisZFLU")
+
+interstellar.show_trailer()
